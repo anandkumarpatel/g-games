f7bb58cc8b0b090393adc19803510ff72d8350f0
Anand Patel <anandkumarpatel@gmail.com>
2024-05-29 20:18:40 -0700

    Update Routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,35 @@
 import { Stack } from 'expo-router'
-import { View } from 'react-native'
-import { Text } from 'react-native-paper'
 import CustomNavigationBar from './AppBar'
 
 export default function RootLayout() {
+  // return <Stack />
   return (
     <Stack
-      initialRouteName='index'
-      screenOptions={{
-        header: (props) => <CustomNavigationBar {...props} />,
-      }}
+    // initialRouteName='index'
+    // screenOptions={{
+    //   header: (props) => <CustomNavigationBar {...props} />,
+    // }}
     >
-      <Stack.Screen name='index' />
-      <Stack.Screen name='match' />
+      <Stack.Screen
+        options={{
+          title: 'Win',
+          headerShown: false,
+        }}
+        name='win'
+      />
+      <Stack.Screen
+        name='index'
+        options={{
+          title: 'Game List',
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name='match'
+        options={{
+          title: 'Matching Game',
+        }}
+      />
     </Stack>
   )
 }
--- app/win.tsx
+++ app/win.tsx
@@ -2,22 +2,35 @@ import { Link } from 'expo-router'
 import { View } from 'react-native'
 import { Button, Text } from 'react-native-paper'
 
-export default function Index() {
+export default function Win() {
   return (
     <View
       style={{
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
+        gap: 10,
+        padding: 20,
+        flexDirection: 'column',
       }}
     >
-      <Text>You Won!</Text>
-      <Link href={{ pathname: 'match' }} asChild>
-        <Button mode='contained'>Play Again</Button>
-      </Link>
-      <Link asChild href={{ pathname: 'index' }}>
-        <Button mode='contained'>Play Different Game</Button>
-      </Link>
+      <Text
+        style={{
+          fontSize: 100,
+          alignSelf: 'center',
+        }}
+      >
+        You Won!
+      </Text>
+      <View>
+        <Link href={{ pathname: 'match' }} asChild>
+          <Button mode='contained'>Play Again</Button>
+        </Link>
+      </View>
+      <View>
+        <Link asChild href={{ pathname: '/' }}>
+          <Button mode='contained'>Play Different Game</Button>
+        </Link>
+      </View>
     </View>
   )
 }
